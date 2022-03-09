b04072331af3121a03143da6f88ee95cbb89def6
Kaz <takaik@yahoo.com>
2022-03-09 13:19:30 -0500

    db:seed, create routes =>  products show, index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show]
+
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def show
+    render json: @product.as_json
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:id, :name, :price, :description)
+  end
+
 end
