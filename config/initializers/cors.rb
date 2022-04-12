eb7a363cfc67b92d05afbce2060b86da60ce99d6
Kaz <takaik@yahoo.com>
2022-04-12 13:50:07 -0400

    change CORS setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://mini-capstone.peterxjang.com", "http://localhost:3000" 
+    origins "https://mini-capstone.peterxjang.com", "http://localhost:8080" 
 
     resource "*",
       headers: :any,
